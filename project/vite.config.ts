b6066a9d13b5f9f7f180ba8a533b953bcbd8e372
MBarry01 <barrymohamadou98@gmaail.com>
2025-01-23 17:15:59 +0100

    Test du déploiement avec GitHub Actions

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Permet le déploiement sur GitHub Pages
+  base: '/VIHData/', // Permet le déploiement sur GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react']
   },
